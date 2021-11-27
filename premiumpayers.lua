34893f838cf06b45605f65382172ff3e62a9c0a9
I2345 <57865481+I2345@users.noreply.github.com>
2021-11-27 10:33:39 +0000

    Update premiumpayers.lua

--- premiumpayers.lua
+++ premiumpayers.lua
@@ -1,4 +1,13 @@
-local playeruserIDs = {
-  [571393370] = false;
-}
-return playeruserIDs
+local module = {}
+local paid = {}
+
+
+function module:CheckPlayer(userid)
+  if paid[userid] then
+    return true
+  else
+    return false
+  end
+end
+
+return module
