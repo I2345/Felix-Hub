ad6bbc63e97b10cc4a6b4eab25909980ed36d6b0
I2345 <57865481+I2345@users.noreply.github.com>
2021-12-07 16:27:32 +0000

    Create Whitelist.lua

---
+++ Whitelist.lua
@@ -0,0 +1,16 @@
+local Accesslist = {
+     [571393370] = true;
+    [2840461340] = true;
+    [1528381087] = true;
+    [168088505] = true;
+    [1559023528] = true;
+    [2844484038] = true;
+    [252646421] = true;
+    [11115979] = true;
+    [3940363] = true;
+    [1852305382] = true;
+    [1185812313] = true;
+}
+
+getgenv().AccessList = Accesslist
+print("Loaded")
