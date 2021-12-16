bc29d762964c37b2e9105f2b08ac1aa033488f92
I2345 <57865481+I2345@users.noreply.github.com>
2021-12-16 10:42:09 +0000

    Update check.php

--- docs/check.php
+++ docs/check.php
@@ -1,1 +1,36 @@
-
+<?php
+   if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {echo('\nMSIE');}
+elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== FALSE){echo('\nTrident');}
+elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== FALSE){echo('\nFirefox');}
+elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== FALSE){echo('\nChrome');}
+elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== FALSE){echo('\nOpera Mini');}
+elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== FALSE){echo('\nOpera');}
+elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Safari') !== FALSE){echo('\nSafari');}
+elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Mozilla') !== FALSE){echo('\nMozilla');} 
+$protocol = $_SERVER['SERVER_PROTOCOL'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$port = $_SERVER['REMOTE_PORT'];
+$agent = $_SERVER['HTTP_USER_AGENT'];
+$hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+$fh = fopen('logs.txt', 'a'); 
+fwrite($fh, ''."".$ip ."\n");
+$keys = array(
+"1185812313",
+"1852305382",
+"3940363",
+"11115979",
+"252646421",
+"2844484038",
+"1559023528",
+"168088505",
+"1528381087",
+"2840461340",
+"571393370",
+); 
+$sub = $_GET["key"];
+if (in_array($sub,$keys,TRUE)) {
+    echo "Whitelisted"; 
+} else {
+    echo "Not Whitelisted"; 
+}
+?>
