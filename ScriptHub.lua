4023ee64485d5dec6e3988a158ab7e76736379ab
I2345 <57865481+I2345@users.noreply.github.com>
2021-12-16 11:41:15 +0000

    Update ScriptHub.lua

--- ScriptHub.lua
+++ ScriptHub.lua
@@ -1,1 +1,7 @@
+local SupportedGames = {
+    {"SimplySpy", "https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua", "Universal"};
+    {"Something Hub", "https://somethinghub.xyz/script.lua", "Pet Simulator X"};
+    {"Recoil Cheats", "https://raw.githubusercontent.com/mrgunz/S/main/Recoil%20-%20Aim%20Assist%20(Chest)", "Recoil"};
+}
 
+getgenv().ScriptHub = SupportedGames
