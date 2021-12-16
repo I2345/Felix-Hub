786a3e2672dec548f9324c7cd39c940750dbff6d
I2345 <57865481+I2345@users.noreply.github.com>
2021-12-16 13:13:25 +0000

    Update ScriptHub.lua

--- ScriptHub.lua
+++ ScriptHub.lua
@@ -6,7 +6,7 @@ local SupportedGames = {
     {"Legit Phantom Forces Cheat", "https://ducksite.xyz/ceri/main.lua", "Phantom Forces"};
     {"Blackhawk Rescue Mission 5 AutoFarm", "https://raw.githubusercontent.com/AlexR32/Roblox/main/BRM5/Autofarm.lua", "Blackhawk Rescue Mission 5"};
     {"Blackhawk Rescue Mission 5 UI", "https://raw.githubusercontent.com/AlexR32/Roblox/main/BRM5/SilentAim.lua", "Blackhawk Rescue Mission 5"};
-    {"Mad City Autfarm", "https://raw.githubusercontent.com/MadGics/Scripty/main/M4d.C", "Mad City"};
+    {"Mad City Autofarm", "https://raw.githubusercontent.com/MadGics/Scripty/main/M4d.C", "Mad City"};
     {"Strongest Punch Simulator AutoFarm", "https://raw.githubusercontent.com/NotOnTop/Exploits_Public/main/StrongestPunchSimulator.lua", "Strongest Punch Simulator"};
     {"Freecam script", "https://pastebin.com/raw/yJ51HKy9", "Universal"};
 }
