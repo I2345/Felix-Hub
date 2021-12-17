47f8adc1f2b48404b17d72bf021e5d16f4263c58
I2345 <57865481+I2345@users.noreply.github.com>
2021-12-17 11:22:33 +0000

    Update ScriptHub.lua

--- ScriptHub.lua
+++ ScriptHub.lua
@@ -1,6 +1,6 @@
 local SupportedGames = {
     {"SimplySpy", "https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua", "Universal"};
-    {"Something Hub", "https://somethinghub.xyz/script.lua", "Pet Simulator X"};
+    {"Something Hub(need Premium Something hub)", "https://somethinghub.xyz/script.lua", "Pet Simulator X"};
     {"Recoil Cheats", "https://raw.githubusercontent.com/mrgunz/S/main/Recoil%20-%20Aim%20Assist%20(Chest)", "Recoil"};
     {"FE Revis Admin", "https://pastebin.com/raw/Caniwq2N", "Universal"};
     {"Legit Phantom Forces Cheat", "https://ducksite.xyz/ceri/main.lua", "Phantom Forces"};
@@ -8,7 +8,18 @@ local SupportedGames = {
     {"Blackhawk Rescue Mission 5 UI", "https://raw.githubusercontent.com/AlexR32/Roblox/main/BRM5/SilentAim.lua", "Blackhawk Rescue Mission 5"};
     {"Strongest Punch Simulator AutoFarm", "https://raw.githubusercontent.com/NotOnTop/Exploits_Public/main/StrongestPunchSimulator.lua", "Strongest Punch Simulator"};
     {"Freecam script", "https://pastebin.com/raw/yJ51HKy9", "Universal"};
-    {"Mad City Autofarm", "https://raw.githubusercontent.com/ThisUsernameWasObfuscatedUsingPSUObf40A/Scripts/main/MadCityAutoRob", "Mad City"}
+    {"Mad City Autofarm", "https://raw.githubusercontent.com/ThisUsernameWasObfuscatedUsingPSUObf40A/Scripts/main/MadCityAutoRob", "Mad City"};
+    {"Dark Dex", "https://gist.githubusercontent.com/DinosaurXxX/b757fe011e7e600c0873f967fe427dc2/raw/ee5324771f017073fc30e640323ac2a9b3bfc550/dark%2520dex%2520v4", "Universal"};
+    {"Auto Midi File Piano Player", "https://raw.githubusercontent.com/richie0866/MidiPlayer/main/package.lua", "Universal"};
+    {"Bloxtap v3", "https://raw.githubusercontent.com/I2345/FelixHubScriptHub/main/Bloxtap%20v3.lua", "Counter Blox"};
+    {"Hexagon", "https://raw.githubusercontent.com/I2345/FelixHubScriptHub/main/Hexagon.lua", "Counter Blox"};
+    {"Username Spoffer", "https://raw.githubusercontent.com/I2345/FelixHubScriptHub/main/Usernamespoffer.lua", "Counter Blox"};
+    {"Generic Aimbot", "https://raw.githubusercontent.com/I2345/FelixHubScriptHub/main/CBRO/%5BFE%5D%20Aimbot.txt", "Counter Blox"};
+    {"Generic UI", "https://raw.githubusercontent.com/I2345/FelixHubScriptHub/main/CBRO/%5BFE%5D%20Counter%20Blox%20Roblox%20Offensive%20Gui.txt", "Counter Blox"};
+    {"UI", "https://raw.githubusercontent.com/I2345/FelixHubScriptHub/main/RecoilUnlockallSkins.lua", "Recoil"};
+    {"Explode", "https://raw.githubusercontent.com/I2345/FelixHubScriptHub/main/Work%20at%20Pizza%20Place/%5BFE%5D%20Explode.txt", "Work at a Pizza Place"};
+    {"Remove Jihad", "https://raw.githubusercontent.com/I2345/FelixHubScriptHub/main/Work%20at%20Pizza%20Place/%5BFE%5D%20Remove%20Jihad.txt", "Work at a Pizza Place"};
+    {"Terroist Bomb", "https://raw.githubusercontent.com/I2345/FelixHubScriptHub/main/Work%20at%20Pizza%20Place/%5BFE%5D%20Terrorist%20Bomber.txt", "Work at a Pizza Place"};
 }
 
 getgenv().ScriptHub = SupportedGames
