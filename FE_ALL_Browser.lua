56e4fa649630de73e5cfaed9276f05d33e0741f6
I2345 <57865481+I2345@users.noreply.github.com>
2021-12-17 11:11:14 +0000

    Create FE_ALL_Browser.lua

---
+++ FE_ALL_Browser.lua
@@ -0,0 +1,5 @@
+local supportedgames = {
+    {"Friend Request Spam", "https://raw.githubusercontent.com/I2345/FelixHubScriptHub/main/R6/%5BFE%5D%20Friend%20Request%20Spam.txt"};
+}
+
+getgenv().FEALLBROWSER = supportedgams
