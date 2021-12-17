264cbdd5f914a5a4ff5c1b792410dadc74f58cd7
I2345 <57865481+I2345@users.noreply.github.com>
2021-12-17 11:10:31 +0000

    Create R6_Script_Browser.lua

---
+++ R6_Script_Browser.lua
@@ -0,0 +1,6 @@
+local supportedgames = {
+    {"Black Hole", "https://raw.githubusercontent.com/I2345/FelixHubScriptHub/main/R6/%5BFE%5D%20Blackhole.txt"};
+    {"Block Hats", "https://raw.githubusercontent.com/I2345/FelixHubScriptHub/main/R6/%5BFE%5D%20Block%20Hats.txt"};
+}
+
+getgenv().R6Browser = supportedgames
