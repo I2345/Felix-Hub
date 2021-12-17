2e32ded880e42660b2391d1cb20ac1fcd8c3187a
I2345 <57865481+I2345@users.noreply.github.com>
2021-12-17 11:10:48 +0000

    Create R15_Script_Browser.lua

---
+++ R15_Script_Browser.lua
@@ -0,0 +1,5 @@
+local supportedgames = {
+   {"Amongus Character", "https://raw.githubusercontent.com/I2345/FelixHubScriptHub/main/R15/%5BFE%5D%20R15%20Cut.txt"}; 
+}
+
+getgenv().R15Browser = supportedgames
